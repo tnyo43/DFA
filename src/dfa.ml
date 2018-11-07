f8b09d4d21fbd375cadaecbc4ebd9cf0c04480ae
tyno43 <skriulle5@gmail.com>
2018-11-07 21:35:19 +0900

    add: normalize function

--- src/dfa.ml
+++ src/dfa.ml
@@ -8,6 +8,8 @@ let alpha_to_num a =
   | One -> 1
 ;;
 
+let alpha_to_string a = string_of_int @@ alpha_to_num a;;
+
 type state = S of string * bool * string list;;
 exception InvalidState of string;;
 
@@ -57,4 +59,123 @@ let is_accepting_language (sts: state list) (lang: alphabet list): bool =
   let st = get_state sts "s" in
   is_accepting @@ transition_state sts st lang
 ;;
+
+(* 正規表現のタイプ。省略形は"leg" *)
+type legex =
+  | State of string
+  | Alpha of alphabet
+  | Epsilon (* 長さ0の言語 *)
+  | Empty (* 空集合 *)
+  | Union of legex list (* 積 *)
+  | Concat of legex list (* 接続 *)
+  | Star of legex (* 0回以上の繰り返し *)
+;;
+
+let legex_type_to_num lgx =
+  match lgx with
+  | Empty -> (-1)
+  | Epsilon -> 0
+  | State _ -> 1
+  | Alpha _ -> 2
+  | Union _ -> 3
+  | Concat _ -> 4
+  | Star _ -> 5
+;;
+
+let rec compare_legex lgx1 lgx2 =
+  let n1 = legex_type_to_num lgx1 in
+  let n2 = legex_type_to_num lgx2 in
+  if n1 < n2 then (-1)
+  else if n1 > n2 then 1
+  else
+    let rec compare_legex_list lst1 lst2 =
+      match lst1 with
+      | [] -> if lst2 = [] then 0 else 1
+      | h::t ->
+          if lst2 = [] then (-1)
+          else 
+            let res = compare_legex h @@ List.hd lst2 in
+            if res = 0 then compare_legex_list t @@ List.tl lst2
+            else 0
+    in
+    match lgx1, lgx2 with
+    | State x1, State x2 -> if x1 < x2 then (-1) else 1
+    | Alpha x1, Alpha x2 -> if (alpha_to_string x1) < (alpha_to_string x2) then (-1) else 1
+    | Union lst1, Union lst2 -> compare_legex_list lst1 lst2
+    | Concat lst1, Concat lst2 -> compare_legex_list lst1 lst2
+    | _ -> failwith "something wrong"
+;;
+
+let rec sort_legex_list (lst: legex list): legex list = 
+  List.sort compare_legex @@ List.map sort_if_lgx_list lst
+and  sort_if_lgx_list lgx =
+  match lgx with
+  | Union lst -> Union (sort_legex_list lst)
+  | Concat lst -> Concat (List.map sort_if_lgx_list lst)
+  | Star x -> Star (sort_if_lgx_list x)
+  | x -> x
+;;
+
+let unite_legex_list lst =
+  match lst with
+  | [] -> Empty
+  | h::[] -> h
+  | h::t -> Union lst
+;;
+
+let unite_legex l1 l2 =
+  match l1 with
+  | Empty -> l2
+  | Union lst1 ->
+      begin
+        match l2 with
+        | Empty -> l1
+        | Union lst2 -> Union (lst1 @ lst2)
+        | x -> unite_legex_list (x::lst1)
+      end
+  | x -> 
+    begin
+        match l2 with
+        | Empty -> l1
+        | Union lst2 -> unite_legex_list (x::lst2)
+        | m -> Union [l1; l2]
+    end
+;;
+
+let concatinate_legex l1 l2 =
+  match l1 with
+  | Epsilon -> l2
+  | Empty -> Empty
+  | Concat x -> 
+      begin
+        match l2 with
+        | Epsilon -> l1
+        | Empty -> Empty
+        | Concat y -> Concat (x@y)
+        | y -> Concat (x@[y])
+      end
+  | x ->
+      begin 
+        match l2 with
+        | Epsilon -> l1
+        | Empty -> Empty
+        | Concat y -> Concat (x::y)
+        | y -> Concat ([x;y])
+      end
+;;
+
+let normalize_legex lgx =
+  let rec normalize_legex lgx =
+    match lgx with
+    | Union [l] -> normalize_legex l
+    | Union lst -> sort_if_lgx_list @@ List.fold_right (fun x -> (fun res -> (unite_legex (normalize_legex x) res))) lst Empty
+    | Concat [l] -> normalize_legex l
+    | Concat lst -> sort_if_lgx_list @@ List.fold_right (fun x -> (fun res -> (concatinate_legex (normalize_legex x) res))) lst Epsilon
+    | x -> x
+  in
+  sort_if_lgx_list @@ normalize_legex lgx
+;;
+
+
+
 
--- tests/dfa_test.ml
+++ tests/dfa_test.ml
@@ -21,7 +21,7 @@ let states = [
 ];;
 
 let state_test =
-  "state name, accepting and path" >::
+  "stateの各値を取得する" >::
     (fun _ ->
       assert_equal "q0" (Dfa.get_state_name st1);
       assert_equal false (Dfa.is_accepting st2);
@@ -30,16 +30,16 @@ let state_test =
 ;;
 
 let get_state_test =
-  "get state from states" >::
+  "stateの名前を探す。ない時はException" >::
     (fun _ ->
-      assert_equal (List.nth states 1) st1;
-      assert_equal (List.nth states 5) st2;
+      assert_equal st1 (Dfa.get_state states "q0");
+      assert_equal st2 (Dfa.get_state states "q4");
       assert_raises (Dfa.InvalidState "hoge") (fun _ -> Dfa.get_state states "hoge");
     )
 ;;
 
 let transition_test =
-  "transition to other state" >::
+  "statesの経路を辿った状態を返す" >::
     (fun _ ->
       assert_equal (Dfa.get_state states "q1") (Dfa.transition_state states (Dfa.get_state states "q0") [One]);
       assert_equal (Dfa.get_state states "q4") (Dfa.transition_state states (Dfa.get_state states "q0") [One; Zero; Zero; One]);
@@ -47,12 +47,37 @@ let transition_test =
 ;;
 
 let accepting_language_test =
-  "accept language" >::
+  "statesの経路を辿って受理されるか否か" >::
     (fun _ ->
       assert_equal true (Dfa.is_accepting_language states [ One; One; One; One]);
       assert_equal false (Dfa.is_accepting_language states [ One; One; One; One; One]);
     )
 ;;
+
+let sort_legex_test =
+  "legexのリストを統一された順番でソートする。意味が異なってはいけない" >::
+    (fun _ ->
+      assert_equal [Dfa.Empty; Union [Epsilon; Alpha Zero]; Concat [State "b"; State "a"]; Star (Union [State "a"; Alpha One; Concat [Empty]])] (Dfa.sort_legex_list [ Star(Union[Alpha One; State "a"; Concat [Empty]]); Concat [ State "b"; State "a" ]; Union [ Epsilon; Alpha Zero]; Empty]);
+      assert_equal [Dfa.Concat [State "b"; State "c"; State "a"; Union [State "a"; State "b"; State "c"]]] (Dfa.sort_legex_list [Concat [State "b"; State "c"; State "a"; Union [State "b"; State "c"; State "a"]]]);
+    )
+;;
+
+let concatinate_test =
+  "legexをconcatinateするとき、意味を変えずに冗長なかっこが増えないようにする" >::
+    (fun _ ->
+      assert_equal (Dfa.Concat [Dfa.Union [Alpha Zero; Alpha One]; State "a"]) (Dfa.concatinate_legex (Dfa.Union [Alpha Zero; Alpha One]) (State "a"));
+      assert_equal (Dfa.Concat [Dfa.Alpha Zero; Dfa.Alpha One; Dfa.Alpha Zero]) (Dfa.concatinate_legex (Dfa.Alpha Zero) (Concat [Dfa.Alpha One; Dfa.Alpha Zero]));
+      assert_equal (Dfa.Empty) (Dfa.concatinate_legex Dfa.Epsilon Dfa.Empty);
+    )
+;;
+
+let normalize_legex_test =
+  "冗長な包括をなくす。また、Emptyの処理をうまくする" >::
+    (fun _ ->
+      assert_equal (Dfa.Alpha Zero) (Dfa.normalize_legex (Dfa.Union [Alpha Zero]));
+      assert_equal (Dfa.Union [Alpha Zero; Alpha One]) (Dfa.normalize_legex (Dfa.Union [Dfa.Union[Dfa.Alpha Zero]; Dfa.Concat[Dfa.Alpha One]]));
+      assert_equal (Dfa.Empty) (Dfa.normalize_legex (Dfa.Concat [Dfa.Union [Alpha One; Alpha Zero]; Dfa. Empty]))
+    )
 
 let tests =
   "all_tests" >::: [ 
@@ -60,4 +85,7 @@ let tests =
     state_test;
     get_state_test;
     accepting_language_test;
+    sort_legex_test;
+    concatinate_test;
+    normalize_legex_test;
   ];;
